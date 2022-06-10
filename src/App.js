4a305984d80672103dc250c53db8a434fb47dea2
taehyeong park <b_tae96@naver.com>
2022-06-10 16:38:35 +0900

    fix . 전역 CSS 파일 수정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+//컴포넌트
+import { Login } from "./route/Login";
+
 function App() {
   return (
     <div className="App">
-      <p>기본세팅중</p>
+      <Login />
     </div>
   );
 }
