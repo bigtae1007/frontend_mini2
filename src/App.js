9ea58e7ad46b402024c9a15df0740da214a155cd
taehyeong park <b_tae96@naver.com>
2022-06-10 19:45:35 +0900

    update . 로그인 페이지 작성
    로그인 페이지 작성, 로그인 버튼 막기 작성, 로그인 버튼에 submit 연결 완료, 로그인 후 메인 페이지 이동 외 링크 연결 완료

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import logo from "./logo.svg";
 import "./App.css";
-
+import { Route, Routes } from "react-router-dom";
+//CSS
+import GlobalStyle from "./elems/GlobalStyle";
 //컴포넌트
-import { Login } from "./route/Login";
+import Login from "./route/Login";
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <GlobalStyle />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/features/login/LoginForm.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+// 컴포넌트
+import Input from "../../elems/Input";
+import Button from "../../elems/Button";
+
+export default function LoginForm() {
+  // 버튼 잠금 state
+  const [formstate, setFromState] = useState(false);
+  // data 입력 state
+  const [loginData, setloginData] = useState({ email: "", password: "" });
+
+  //input 데이터 저장하기
+  const changeInput = (e) => {
+    const { value, id } = e.target;
+    setloginData({ ...loginData, [id]: value });
+  };
+
+  // submit 이벤트
+  const submitLogin = (e) => {
+    e.preventDefault();
+  };
+
+  React.useEffect(() => {
+    // 버튼 잠금
+    if (loginData.email !== "" && loginData.password !== "") {
+      setFromState(true);
+    } else {
+      setFromState(false);
+    }
+  }, [loginData]);
+
+  return (
+    <WrapForm onSubmit={submitLogin}>
+      <Input
+        id="email"
+        type="email"
+        placeholder="이메일을 입력"
+        required
+        onChange={changeInput}
+      />
+      <Input
+        id="password"
+        placeholder="비밀번호 입력"
+        required
+        onChange={changeInput}
+      />
+      <Button
+        type="submit"
+        size="size1"
+        bgcolor={formstate ? "blue" : "grey"}
+        color={formstate ? "white" : "black"}
+        disabled={!formstate}
+      >
+        로그인
+      </Button>
+    </WrapForm>
+  );
+}
+
+const WrapForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
