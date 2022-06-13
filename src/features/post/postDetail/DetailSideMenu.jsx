5fa21fdcb7fa4b0ebbfab65010f281afd00c0222
junguk11 <kjuzxcvbnm@naver.com>
2022-06-13 16:09:07 +0900

    update. 수정 삭제하기 버튼추가 아직 조건부 안됨

--- src/features/post/postDetail/DetailSideMenu.jsx
+++ src/features/post/postDetail/DetailSideMenu.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import styled from "styled-components";
 
 export default function DetailSideMenu() {
+  const user_data = useSelector((state) => state.login.user);
+  console.log(user_data);
   return (
     <>
       <SideMenuDiv>
@@ -14,6 +17,8 @@ export default function DetailSideMenu() {
 
         <p>like : 20</p>
         <p>해결 완료</p>
+        <EditBtn>Edit</EditBtn>
+        <DeleteBtn>Delete</DeleteBtn>
       </SideMenuDiv>
     </>
   );
@@ -30,4 +35,24 @@ const SideMenuDiv = styled.div`
 
   border-left: 1px solid var(--grey);
   word-break: break-all;
+`;
+
+const EditBtn = styled.button`
+  width: 80px;
+  border: none;
+  margin: 0 auto;
+  padding: 3px 5px;
+  border-radius: 10px;
+  background-color: var(--green);
+  color: white;
+`;
+
+const DeleteBtn = styled.button`
+  margin: 0 auto;
+  width: 80px;
+  border: none;
+  padding: 3px 5px;
+  border-radius: 10px;
+  background-color: var(--red);
+  color: white;
 `;
